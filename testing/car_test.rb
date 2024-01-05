670a1c0d9e64d370b3372bb7744c6be3999824a0
EC2 Default User <ec2-user@ip-172-31-25-25.us-west-1.compute.internal>
2024-01-05 01:57:00 +0000

    testing files

---
+++ testing/car_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'car'
+
+class CarTest < Minitest::Test
+  def test_wheels
+    car = Car.new
+    assert_equal(car.wheels, 4)
+  end
+end
